01ec57646158e0503793097c4942cc1f3e3b30c6
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-11-30 21:51:20 -0500

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,17 +1,16 @@
 # Set variables:
 $temp = $env:Temp
-$folderName = "pc-flipper-script"
 
 # Changes directory to the Windows temp folder.
 Set-Location $temp
 
-# Deletes old Windows Toolbox files to avoid conflicts if you've run the script before.
-Remove-Item -Recurse -Force -Confirm:$false "$folderName"
-# Creates new directory for UltimateWindowsToolbox's files and scripts
-New-Item -Name "$folderName" -Type Directory
+# Deletes old files to avoid conflicts if you've run the script before.
+Remove-Item -Recurse -Force -Confirm:$false "pc-flipper-script"
+# Creates new directory for files and scripts
+New-Item -Name "pc-flipper-script" -Type Directory
 
-# Sets location to the windows toolbox folder
-Set-Location "$folderName"
+# Sets location to the script folder
+Set-Location "pc-flipper-script"
 
 # Downloads main script
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerPCFan/pc-flipper-windows-script/refs/heads/main/pc-flip-preparation-script.ps1" -OutFile "pc-flip-preparation-script.ps1"

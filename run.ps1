8ea25e6288c9ee3ab22a30fbe4acc4760be3307c
PowerPCFan <charlies.retro.computing@gmail.com>
2025-04-11 18:21:30 -0400

    jdfgkadfkflasjfkldjgskjgskdgjdfsdkg

--- run.ps1
+++ run.ps1
@@ -17,10 +17,14 @@ New-Item -Type Directory -Path "$scriptDownloadPath\mas"
 
 # Main Script
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerPCFan/pc-flipper-windows-script/refs/heads/main/pc-flip-preparation-script.ps1" -OutFile "$scriptDownloadPath\pc-flip-preparation-script.ps1"
+
 # hwid.cmd
 # this ensures that there are CRLF line endings (Windows) not LF (Unix) - for some reason when you download it, it has UNIX/LF line endings
-$result = Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/PowerPCFan/pc-flipper-windows-script/refs/heads/main/mas/hwid.cmd"
-$result.Content.Replace("`n", "`r`n") | Out-File -FilePath "$scriptDownloadPath\mas\hwid.cmd"
+$masUrl = "https://raw.githubusercontent.com/PowerPCFan/pc-flipper-windows-script/refs/heads/main/mas/hwid.cmd"
+$masOutFilePath = "$scriptDownloadPath\mas\hwid.cmd"
+$lfcontent = (Invoke-WebRequest -UseBasicParsing -Uri $masUrl).Content
+$crlfContent = $lfcontent -replace "`r?`n", "`r`n"
+[System.IO.File]::WriteAllText($masOutFilePath, $crlfContent, [System.Text.Encoding]::UTF8)
 
 # Changes PowerShell's execution policy to Unrestricted
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
